2423ad16916d4502314036c8061833887be432ae
Nicholas Allee <42816286+jiggensnaggen@users.noreply.github.com>
2020-02-17 13:02:40 -0600

    added stuff

--- NicksMapReducer/mapper.py
+++ NicksMapReducer/mapper.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+input = open("Us_Accidents_Dec19.txt", "r")
+output = open("mapped.txt","w")
+print("hello")
+for line in input:
+    listOfData = line.strip().split("    ")
+    junk1, junk2, junk3, id, tmc, severity, start_time, end_time, city, state, timezone, visibility(mi), temperature(f), humidity(%), wind_speed(mph), precipitation(in), weather_condition = listOfData
+    print(severity,state,weather_condition)
+    output.write(severity + "\t" + state + "\t" + temperature(f) + "\t" + weather_condition + "\n")
+
+input.close()
+output.close()
