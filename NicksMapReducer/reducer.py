14e62b90419f55070f4c03df2badbd6f945c4eb4
Nicholas Allee <42816286+jiggensnaggen@users.noreply.github.com>
2020-02-17 13:43:55 -0600

    updated reducer with initial goals

---
+++ NicksMapReducer/reducer.py
@@ -0,0 +1,8 @@
+""" this will take in sorted.txt and find the cities with the most reoccuring temperature.
+
+to do this i will take in each line and check to see if the temp is not in a dictionary. if not then i will put it in the dictionary
+i will also add a list of cities and a count
+then for each line if its the same temp then check the city. if its a different city then add it the list of cities. also add 1 its count
+
+"""
+
