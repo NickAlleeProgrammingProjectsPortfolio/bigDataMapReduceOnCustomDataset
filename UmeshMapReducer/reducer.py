466da241ca7e6d12703c91cdc9e85265aab722bd
Umesh Singh <s530529@nwmissouri.edu>
2020-02-21 14:06:51 -0600

    For the count of weather condition

---
+++ UmeshMapReducer/reducer.py
@@ -0,0 +1,27 @@
+unreduced = open("sorted.txt","r")
+reduced = open("reduced.txt", "w")
+
+thisKey = ""
+count = 0
+
+for line in unreduced:
+  data = line.strip().split('\t')
+  state, weather_condition = data
+
+  if weather_condition != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      reduced.write(thisKey + '\t' + str(count)+'\n')
+
+    # start over when changing keys
+    thisKey = weather_condition
+  
+  # apply the aggregation function
+  # thisValue += int(severity)
+  count += 1
+
+# output the final entry when done
+reduced.write(thisKey + '\t' + str(count)+'\n')
+
+unreduced.close()
+reduced.close()
